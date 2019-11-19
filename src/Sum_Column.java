02f8bc9762a7e3c5ba7422856b2f32248831900c
mmiran11 <mmiran11@J26307.win.kennesaw.edu>
2019-11-19 12:21:27 -0500

    created scanner.
    created new 3x4 array, array. added loop skeleton to fill array

---
+++ src/Sum_Column.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Sum_Column {
+
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		int[][] array = new int[3][4];
+		for (int i = 0; i < array.length; i++) 
+		{
+			
+		}
+	}
+	
+	public void sumColumn(int[][] arr) 
+	{
+		
+	}
+
+}
--- src/sumColumn.java
+++
@@ -1,9 +0,0 @@
-
-public class sumColumn {
-
-	public static void main(String[] args) {
-		
-
-	}
-
-}
