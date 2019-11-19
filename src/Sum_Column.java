b3c4d720fb21078a58f876eb5417cca962f8c4e3
mmiran11 <mmiran11@J26307.win.kennesaw.edu>
2019-11-19 14:12:42 -0500

    loop now fills 2d array by taking values from user.
    original loop is outer loop for rows. inner loop is for columns. created
    a second nested loop to print values from 2d array produced from first
    nested loop

--- src/Sum_Column.java
+++ src/Sum_Column.java
@@ -5,15 +5,30 @@ public class Sum_Column {
 	{
 		Scanner scan = new Scanner(System.in);
 		int[][] array = new int[3][4];
-		for (int i = 0; i < array.length; i++) 
+		for (int row = 0; row < array.length; row++) //outer loop is current row
 		{
-			
+			for (int column = 0; column < array[0].length; column++) //inner loop is current column
+			{
+				int values; //variable to hold user values
+				System.out.print("Enter a value: "); //prompt for matrix values
+				values = scan.nextInt(); //the inputed value
+				array[row][column] = values; //accessing current row index and assign value to current column index
+			}
+		}
+		System.out.println("The entered matrix: ");
+		for (int[] row : array) //for each array in the matrix (which the rows represent)
+		{
+		   for (int column : row) //for which column the current row is in
+		   {
+		        System.out.print(column + " "); //prints out value in current row and column
+		   }
+		   System.out.println(); //prints a new line after current array/row has its values printed
 		}
 	}
 	
-	public void sumColumn(int[][] arr) 
-	{
-		
-	}
+//	public void sumColumn(int[][] arr) 
+//	{
+//		
+//	}
 
 }
